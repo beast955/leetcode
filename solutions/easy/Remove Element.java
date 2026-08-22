0101c829e65f07e3c75122985704cbfd12e126fe
Aayush Kathuria <136057011+beast955@users.noreply.github.com>
2026-08-22 17:02:42 +0530

    Add solution for Remove Element

---
+++ solutions/easy/Remove Element.java
@@ -0,0 +1,17 @@
+// Title: Remove Element
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/remove-element/
+
+class Solution {
+    public int removeElement(int[] arr, int val) {
+        int size=arr.length;
+        int k=0;
+        for(int i=0;i<size;i++){
+            if(arr[i]!=val){
+            }
+        }
+    }
+                arr[k]=arr[i];
+                k++;
+        return(k);
