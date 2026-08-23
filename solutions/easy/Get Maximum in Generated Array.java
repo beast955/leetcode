77a95267a0b54f60db815a3fe1dd4e7f5f1c7301
Aayush Kathuria <136057011+beast955@users.noreply.github.com>
2026-08-23 12:12:17 +0530

    Add solution for Get Maximum in Generated Array

---
+++ solutions/easy/Get Maximum in Generated Array.java
@@ -0,0 +1,15 @@
+// Title: Get Maximum in Generated Array
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/get-maximum-in-generated-array/
+
+    if (nums[j] > max) {
+        max = nums[j];
+    }
+    
+    }
+  return max;
+    }
+}
+for (int j = 1; j < nums.length; j++) {
+        }    int max = nums[0];
