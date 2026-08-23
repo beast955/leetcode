ea027243079b71140ebae5bcbda2f91d8f2603fd
Aayush Kathuria <136057011+beast955@users.noreply.github.com>
2026-08-23 11:49:43 +0530

    Add solution for Concatenation of Array

---
+++ solutions/easy/Concatenation of Array.py
@@ -0,0 +1,9 @@
+// Title: Concatenation of Array
+            // Difficulty: Easy
+            // Language: Python
+            // Link: https://leetcode.com/problems/concatenation-of-array/
+
+class Solution(object):
+    def getConcatenation(self, nums):
+       c=nums*2
+       return c
