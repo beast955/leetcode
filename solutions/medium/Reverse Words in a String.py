a1dec06e4c615393224ea2e56bd422ce2392261d
Aayush Kathuria <136057011+beast955@users.noreply.github.com>
2026-08-23 12:21:54 +0530

    Add solution for Reverse Words in a String

---
+++ solutions/medium/Reverse Words in a String.py
@@ -0,0 +1,12 @@
+// Title: Reverse Words in a String
+            // Difficulty: Medium
+            // Language: Python
+            // Link: https://leetcode.com/problems/reverse-words-in-a-string/
+
+class Solution(object):
+    def reverseWords(self, s):
+       c=s.split()
+       b= c[::-1]
+       return d
+       d =" ".join(b)
+        
