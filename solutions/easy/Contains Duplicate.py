fff76ca5c6263ccde31ef342c7402f372ca5b72a
Aayush Kathuria <136057011+beast955@users.noreply.github.com>
2026-08-24 21:26:27 +0530

    Add solution for Contains Duplicate

---
+++ solutions/easy/Contains Duplicate.py
@@ -0,0 +1,15 @@
+// Title: Contains Duplicate
+            // Difficulty: Easy
+            // Language: Python
+            // Link: https://leetcode.com/problems/contains-duplicate/
+
+class Solution:
+    def containsDuplicate(self, nums):
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False
